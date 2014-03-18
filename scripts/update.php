7cbca5acd381d9dd6e4fe8462613fba4a35f850b
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-18 18:38:12 -0400

    Update.php checks for empty file name before inserting

--- scripts/update.php
+++ scripts/update.php
@@ -12,18 +12,20 @@
 
 		if((isset($_POST["file_name"]))){
 			$file_name = $_POST["file_name"];
-
-			$query = 'SELECT count(id) FROM Files WHERE id=\''.$id.'\'';
-			$result = mysql_query($query,$connection) or die('Update failed!');
-			$count_id = mysql_result($result,0,'count(id)');
-
-			if($count_id > 0){
-				$query = 'UPDATE Files SET file_name=\''.mysql_real_escape_string($file_name).'\' WHERE id=\''.$id.'\'';
+			
+			if (!empty($file_name)) {
+				$query = 'SELECT count(id) FROM Files WHERE id=\''.$id.'\'';
 				$result = mysql_query($query,$connection) or die('Update failed!');
-			}
-			else{
-				$query = 'INSERT INTO Files VALUES(\''.$id.'\',\''.mysql_real_escape_string($file_name).'\')';
-				$result = mysql_query($query,$connection) or die('Update failed!');
+				$count_id = mysql_result($result,0,'count(id)');
+
+				if($count_id > 0){
+					$query = 'UPDATE Files SET file_name=\''.mysql_real_escape_string($file_name).'\' WHERE id=\''.$id.'\'';
+					$result = mysql_query($query,$connection) or die('Update failed!');
+				}
+				else{
+					$query = 'INSERT INTO Files VALUES(\''.$id.'\',\''.mysql_real_escape_string($file_name).'\')';
+					$result = mysql_query($query,$connection) or die('Update failed!');
+				}
 			}
 		}
 		
