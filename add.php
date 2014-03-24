33c65c3c7bcf0bbf26134695f6137e039689f358
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-23 23:40:29 -0400

    Add more button will now create more movie fields
    Added the JavaScript necessary for the 'Add more' button to function as intended.

--- add.php
+++ add.php
@@ -1,43 +1,45 @@
 <html>
 	<head>
+		<script src="js/openlink.js"></script>
 		<title>Add a movie</title>
 		<?php
 			$qualities = array();
 		?>
 	</head>
 	<body>
-		<form>
-			<input type="text" name="id" placeholder="IMDB ID">
-			<select name="quality">
-				<?php
-					include 'config/config.php';
-					$connection = mysql_connect($HOSTNAME,$USERNAME,$PASSWORD) or die('Connection failed!');
-					mysql_select_db($DATABASE,$connection) or die('Database select failed!');
+		<div id="movies_list">
+			<form>
+				<input type="text" name="id" placeholder="IMDB ID">
+				<select name="quality">
+					<?php
+						include 'config/config.php';
+						$connection = mysql_connect($HOSTNAME,$USERNAME,$PASSWORD) or die('Connection failed!');
+						mysql_select_db($DATABASE,$connection) or die('Database select failed!');
 				
-					$result = mysql_query('SELECT * FROM Quality',$connection);
+						$result = mysql_query('SELECT * FROM Quality',$connection);
 				
-					$numrows = mysql_numrows($result);
-					for($i = 0; $i < $numrows; $i++){
-						$value = mysql_result($result,$i,'quality');
-						array_push($qualities, $value);
-				?>
-						<option value="<?php echo $value ?>"><?php echo $value ?></option>
-				<?php
-					}
-					mysql_close($connection);
-				?>
-			</select>
-			<input type="text" name="file_name" placeholder="File name">
-		</form>
-		<p>&nbsp;</p>
+						$numrows = mysql_numrows($result);
+						for($i = 0; $i < $numrows; $i++){
+							$value = mysql_result($result,$i,'quality');
+							array_push($qualities, $value);
+							?>
+							<option value="<?php echo $value ?>"><?php echo $value ?></option>
+							<?php
+						}
+						mysql_close($connection);
+					?>
+				</select>
+				<input type="text" name="file_name" placeholder="File name">
+			</form>
+		</div>
 		<div>
 			<form style="display:inline" id="save_button" method="post" action="scripts/insert.php" onsubmit="createJSON()">
 				<input type="hidden" name="json" value="">
 				<input type="submit" value="Save">
 			</form>
-			<button style="display:inline">Add more</button>
-			<button style="display:inline">Cancel</button>
+			<button style="display:inline" onclick="addMovie()">Add more</button>
+			<button style="display:inline" onclick="openLink('.')">Cancel</button>
 		</div>
-		<p style="display:none"><?php echo json_encode($qualities); ?></p>
+		<p id="json_holder" style="display:none"><?php echo json_encode($qualities); ?></p>
 	</body>
 </html>
---
+++ js/add.js
@@ -0,0 +1,37 @@
+function addMovie () {
+	var qualities = JSON.parse(document.getElementById('json_holder').innerHTML);
+	var parent = document.getElementById('movies_list');
+	
+	var breakLine = document.createElement('br');
+	var space1 = document.createElement('span');
+	var space2 = document.createElement('span');
+	space1.innerHTML = " ";
+	space2.innerHTML = " ";
+	
+	var idText = document.createElement('input');
+	categoryText.setAttribute('type', 'text');
+	categoryText.setAttribute('name', 'id');
+	categoryText.setAttribute('placeholder', 'IMDB ID');
+	
+	var qualitySelect = document.createElement('select');
+	qualitySelect.setAttribute('name', 'quality');
+	
+	for (var i = 0; i < qualities.length; i++) {
+		var qualOption = document.createElement('option');
+		qualOption.value = qualities[i];
+		qualOption.innerHTML = qualities[i];
+		qualitySelect.appendChild(qualOption);
+	}
+	
+	var filenameText = document.createElement('input');
+	filenameText.setAttribute('type', 'text');
+	filenameText.setAttribute('name', 'file_name');
+	filenameText.setAttribute('placeholder', 'File name');
+	
+	parent.appendChild(breakLine);
+	parent.appendChild(idText);
+	parent.appendChild(space1);
+	parent.appendChild(qualitySelect);
+	parent.appendChild(space2);
+	parent.appendChild(filenameText);
+}
