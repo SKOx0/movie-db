1744d96370521ef57a74918cc33bc3bd28b8f688
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-24 14:53:13 -0400

    Added script for receiving uploaded file
    Added a script that will receive the uploaded backup file.

---
+++ scripts/restore.php
@@ -0,0 +1,20 @@
+<?php
+	$allowedExt = ".mbk";
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$extension = end($temp);
+	if ($extension == $allowedExts) {
+		if ($_FILES["file"]["error"] > 0) {
+			echo "Error: " . $_FILES["file"]["error"] . "<br>";
+		}
+		else
+		{
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			echo "Stored in: " . $_FILES["file"]["tmp_name"];
+		}
+	}
+	else {
+		echo "Invalid file";
+	}
+?>
