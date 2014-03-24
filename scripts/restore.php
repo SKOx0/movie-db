af63b778ebffc510c4d94534a992770a292f830d
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-24 15:05:18 -0400

    Uploaded file gets saved to restore.mbk
    Added code that will enable the uploaded file to be saved in restore.mbk.

--- scripts/restore.php
+++ scripts/restore.php
@@ -4,14 +4,23 @@
 	$extension = end($temp);
 	if ($extension == $allowedExts) {
 		if ($_FILES["file"]["error"] > 0) {
-			echo "Error: " . $_FILES["file"]["error"] . "<br>";
+			echo "Error: ".$_FILES["file"]["error"]."<br>";
 		}
 		else
 		{
-			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-			echo "Type: " . $_FILES["file"]["type"] . "<br>";
-			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-			echo "Stored in: " . $_FILES["file"]["tmp_name"];
+			echo "Upload: ".$_FILES["file"]["name"]."<br>";
+			echo "Type: ".$_FILES["file"]["type"]."<br>";
+			echo "Size: ".($_FILES["file"]["size"] / 1024)." kB<br>";
+			echo "Stored in: ".$_FILES["file"]["tmp_name"];
+			
+			if (file_exists("../restore.mbk")) {
+				exec("rm ../restore.mbk");
+			}
+			
+			move_uploaded_file($_FILES["file"]["tmp_name"], "../restore.mbk");
+			if (file_exists("../restore.mbk")) {
+				echo "Stored in: restore.mbk";
+			}
 		}
 	}
 	else {
