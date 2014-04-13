eb0b9f8e87d132765cd6f7d3b8ec6102609ec2a0
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-13 17:47:36 -0400

    Created basic interface design for v4.0
    Version 4.0 of the Movie DB will have a drastically redesigned interface.

---
+++ new.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/design.css">
+		<title>Movies</title>
+	</head>
+	<body>
+		<div id="header">
+			<table id="header_layout">
+				<tr>
+					<td id="header_left">
+						<div class="header_buttons">
+							<h2 class="header_button_text">&nbsp;&nbsp;Add movie&nbsp;&nbsp;</h2>
+						</div>
+					</td>
+					<td id="header_right">
+						<div>
+							<form>
+								<input id="search_bar" type="text" placeholder="Search...">
+							</form>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div id="body">
+			<div class="movie_box">
+				<img class="movie_poster" src="images/white.jpg" width="150" height="222.222222222">
+				<h3>Movie Name</h3>
+				<p>2014 120mins</p>
+				<p>1080p HD</p>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="footer_content">
+				<p id="footer_text">XXX Movies<br>vX.X.X (branch)<br>&copy; 2013-<span id="copyright_year">2014</span> Viraj Chitnis</p>
+			</div>
+		</div>
+	</body>
+</html>
