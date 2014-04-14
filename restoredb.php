1681ffd4367ea6e884f5b62bf575a46ad00f8bd7
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-14 14:05:39 -0400

    Adding forgotten file

---
+++ restoredb.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Restore from backup</title>
+	</head>
+	<body>
+		<div>
+			<form action="scripts/restore.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="file">
+				<input type="submit" name="submit" value="Restore">
+			</form>
+		</div>
+	</body>
+</html>
