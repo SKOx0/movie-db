b00eaac79afe7a07fcd6a8f99b26ebe53964ce71
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-14 13:58:36 -0400

    Debugging scripts/restore.php

--- scripts/restore.php
+++ scripts/restore.php
@@ -10,25 +10,42 @@
 				exec("rm ../restore.mbk");
 			}
 			
+			echo "Deleted old restore.mbk"."<br>";
+			
 			if (file_exists("../restore")) {
 				exec("rm ../restore");
 			}
+			
+			echo "Deleted old uncompressed restore file"."<br>";
 			
 			if (file_exists("../posters")) {
 				exec("rm -r ../posters");
 			}
 			
+			echo "Delete old posters folder"."<br>";
+			
 			if (file_exists("../movies.sql")) {
 				exec("rm ../movies.sql");
 			}
 			
+			echo "Delete old sql file"."<br>";
+			
 			move_uploaded_file($_FILES["file"]["tmp_name"], "../restore.mbk");
+			
+			echo "Move uploaded file to proper location"."<br>";
+			
 			exec("cd ..; /bin/gunzip -S .mbk restore.mbk");
+			
+			echo "Decompress the restore.mbk file"."<br>";
+			
 			exec("cd ..; /bin/tar -xf restore");
-			include('../config/config.php');
+			
+			echo "Untar the restore file"."<br>";
+			
+			/*include('../config/config.php');
 			exec("cd ..; /usr/bin/mysqlimport -u ".$USERNAME." -p".$PASSWORD." ".$DATABASE." movies.sql");
 			
-			header("Location: ../");
+			header("Location: ../");*/
 		}
 	}
 	else {
