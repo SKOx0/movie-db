c09d8c40d9c4e762761912d0336a6e943f152d73
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-14 13:14:11 -0400

    Debugging addfiles.php

--- scripts/addfiles.php
+++ scripts/addfiles.php
@@ -33,6 +33,7 @@ while($movies_table->fetch()){
 			$link_quality =	"iTunes Movies (SD)";
 		}
 		
+		echo exec("ls ".escapeshellarg("../movies/".$link_quality."/".$name."m4v"));
 		echo $link_quality."<br><br>";
 		
 		if (file_exists("../movies/".$link_quality."/".$name."m4v")) {
