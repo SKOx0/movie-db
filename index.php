38ea51d6cb15c741abab7608a5c1400780ce0c71
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-15 13:39:03 -0400

    Some more iOS webapp meta tags

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
 	?>
 			<meta name="apple-mobile-web-app-title" content="Movies">
 			<meta name="apple-mobile-web-app-capable" content="yes">
-			<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+			<meta name="apple-mobile-web-app-status-bar-style" content="default">
+			<meta name="viewport" content="user-scalable=no, width=device-width">
 	<?php
 		}
 	?>
