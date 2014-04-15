082407c9f5fb46d73fe0c6b8222897b1aab46945
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-14 21:33:45 -0400

    Switched load posters to javascript from jquery

--- js/index.js
+++ js/index.js
@@ -31,24 +31,22 @@ function afterPageLoad () {
 	loadVisiblePosters();
 }
 
-$.fn.isOnScreen = function(){
-    
-    var win = $(window);
-    
-    var viewport = {
-        top : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-    
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-    
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-    
-};
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
 
 $(window).scroll(function() {
 	loadVisiblePosters();
@@ -61,7 +59,7 @@ function writeCopyrightYear () {
 function loadVisiblePosters () {
 	var posters = document.getElementsByClassName('posters');
 	for (var i = 0; i < posters.length; i++) {
-		if ($("#" + posters[i].id).isOnScreen() == true) {
+		if (isElementInViewport(posters[i])) {
 			posters[i].src = "posters/" + posters[i].id + ".jpg";
 			posters[i].style.display = "block";
 		}
