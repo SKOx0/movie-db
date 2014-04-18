b5bf6ade62692b10076fe2bfc553b98e78e1da7c
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-18 12:34:10 -0400

    Fixed syntax error in sendmail function

--- scripts/converter.php
+++ scripts/converter.php
@@ -6,7 +6,7 @@
 	function sendmail($to, $subject, $message) {
 		$headers = "From: \"Movie DB\" <movies@virajchitnis.com>"."\r\n".
 			"MIME-Version: 1.0"."\r\n".
-			"Content-Type: text/html"
+			"Content-Type: text/html";
 
 		mail($to, $subject, $message, $headers);
 	}
