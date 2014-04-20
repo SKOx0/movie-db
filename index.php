b53518f48f28d13d6c5a5c0f0a2bc6fb8cf0d1d2
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-20 00:22:32 -0400

    Added missing file

--- index.php
+++ index.php
@@ -227,7 +227,7 @@
 	</div>
 	<div id="overlay_progress">
 		<div id="ffmpeg_progress"></div>
-		<button onclick="showProgressOverlay()">Close</button>
+		<button onclick="hideProgressOverlay()">Close</button>
 	</div>
 	<div>
 		<table id="header_layout">
---
+++ scripts/getlog.php
@@ -0,0 +1,3 @@
+<?php
+	echo "<pre>".shell_exec("cat ../logs/converter.log")."</pre>";
+?>
