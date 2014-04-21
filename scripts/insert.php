2b402f945e8b2dcb1970d6341c0e443585060816
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-21 18:58:45 -0400

    Insert.php updates movie info if movie already exists in DB
    If the movie already exists in the DB, insert.php treats it as an update.

--- scripts/insert.php
+++ scripts/insert.php
@@ -82,6 +82,34 @@
 				}
 			}
 		}
+		elseif ($count_id > 0) {
+			$db_movies = $db->prepare("UPDATE Movies SET quality = ? WHERE id = ?;");
+			$db_movies->bind_param('ss', $quality, $id);
+			$db_movies->execute();
+			$db_movies->free_result();
+
+			if (!empty($file_name)) {
+				$db_count = $db->prepare("SELECT count(id) FROM Files WHERE id = ?;");
+				$db_count->bind_param('s', $id);
+				$db_count->execute();
+				$db_count->bind_result($count_files_id);
+				$db_count->fetch();
+				$db_count->free_result();
+
+				if($count_files_id > 0){
+					$db_files = $db->prepare("UPDATE Files SET file_name = ? WHERE id = ?;");
+					$db_files->bind_param('ss', $file_name, $id);
+					$db_files->execute();
+					$db_files->free_result();
+				}
+				else{
+					$db_files = $db->prepare("INSERT INTO Files VALUES(?, ?);");
+					$db_files->bind_param('ss', $id, $file_name);
+					$db_files->execute();
+					$db_files->free_result();
+				}
+			}
+		}
 		$db->close();
 	}
 
