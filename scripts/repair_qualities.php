b855848bc0996cfdfa1986832f123a999131bd6d
Viraj Chitnis <chitnisviraj@gmail.com>
2014-05-05 00:07:55 -0400

    Added repair_qualities.php
    Repair_qualities.php compares all movie files with the quality entry in the database and outputs faulty entries.

---
+++ scripts/repair_qualities.php
@@ -0,0 +1,49 @@
+<?php
+include '../config/config.php';
+
+$db = new mysqli($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+function compare_movies ($files, $quality) {
+	global $db;
+	for ($i = 0; $i < count($files); $i++) {
+		if ((substr($files[$i], 0, 1) === '.') || (substr($files[$i], 0, 1) === '_')) {
+			continue;
+		}
+		$files_table = $db->prepare("SELECT id FROM Files WHERE file_name = ?;");
+		$files_table->bind_param('s', $files[$i]);
+		$files_table->execute();
+		$files_table->bind_result($id);
+		$files_table->fetch();
+		$files_table->free_result();
+		
+		$movies_table = $db->prepare("SELECT quality FROM Movies WHERE id = ?;");
+		$movies_table->bind_param('s', $id);
+		$movies_table->execute();
+		$movies_table->bind_result($curr_quality);
+		$movies_table->fetch();
+		$movies_table->free_result();
+		
+		if ($quality != $curr_quality) {
+			/*$update_table = $db->prepare("UPDATE Movies SET quality = ? WHERE id = ?;");
+			$update_table->bind_param('ss', $quality, $id);
+			$update_table->execute();
+			$update_table->free_result();*/
+			echo $files[$i]."						Old: (".$curr_quality.")		New: (".$quality.")<br>";
+		}
+	}
+}
+
+$files_1080p = scandir("../movies/iTunes Movies (1080p HD)/");
+$files_720p = scandir("../movies/iTunes Movies (720p HD)/");
+$files_SD = scandir("../movies/iTunes Movies (SD)/");
+
+compare_movies($files_1080p, "1080p HD");
+compare_movies($files_720p, "720p HD");
+compare_movies($files_SD, "SD");
+
+$db->close();
+?>
